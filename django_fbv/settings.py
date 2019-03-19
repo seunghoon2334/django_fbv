230a2c30fb5fa1049d92960ed884ad0b20496768
gogo12394@naver.com <gogo12394@naver.com>
2019-03-19 00:15:42 +0000

    02|settings.py|

--- django_fbv/settings.py
+++ django_fbv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '09_0)h37x9u(zx4%75p+)!nh1&(5#&#2c^0^vm$(3j&&3m2=ez'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'boards',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
